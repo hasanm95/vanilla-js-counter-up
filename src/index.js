bf2c7ec921f55241cc7cf986239ed54afbbe5394
Hasan Mobarak <hsncool10@gmail.com>
2021-07-15 09:56:35 +0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const wrapper = document.getElementById("funfactId");
+const counters = wrapper.querySelectorAll(".js-counter");
+const duration = 1000;
+
+if (wrapper) {
+  let isCounted = false;
+  document.addEventListener("scroll", function () {
+    const wrapperPos = wrapper.offsetTop - window.innerHeight;
+    if (!isCounted && window.scrollY > wrapperPos) {
+      counters.forEach((counter) => {
+        const countTo = counter.dataset.count;
+
+        const countPerMs = countTo / duration;
+
+        let currentCount = 0;
+        const countInterval = setInterval(function () {
+          if (currentCount >= countTo) {
+            clearInterval(countInterval);
+          }
+          counter.textContent = Math.round(currentCount);
+          currentCount = currentCount + countPerMs;
+        }, 1);
+      });
+      isCounted = true;
+    }
+  });
+}
